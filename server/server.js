e5e2f8d43b542739ec01a9d1b77b348f936f672b
charanrocky <charanrocky441@gmail.com>
2025-06-14 16:47:10 +0530

    added

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const listingRoutes = require('./routes/listingRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const listingRoutes = require("./routes/listingRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const cors = require("cors");
+import path from "path";
 
 dotenv.config();
 connectDB();
@@ -13,9 +14,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/listings', listingRoutes);
-app.use('/api/bookings', bookingRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/listings", listingRoutes);
+app.use("/api/bookings", bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
